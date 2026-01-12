15bea2ad3f0c29ac486b1a3ef0fbad290daa5ac0
Vishal <vishalvpatil45@gmail.com>
2026-01-12 22:01:28 +0530

    Merge Sort

---
+++ Important Sorting Techniques/MergeSort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void merge(int a[], int l, int m, int r) {
+    int n1 = m - l + 1;
+    int n2 = r - m;
+
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++) L[i] = a[l + i];
+    for (int j = 0; j < n2; j++) R[j] = a[m + 1 + j];
+
+    int i = 0, j = 0, k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) a[k++] = L[i++];
+        else a[k++] = R[j++];
+    }
+
+    while (i < n1) a[k++] = L[i++];
+    while (j < n2) a[k++] = R[j++];
+}
+
+void mergeSort(int a[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(a, l, m);
+        mergeSort(a, m + 1, r);
+        merge(a, l, m, r);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    mergeSort(a, 0, n - 1);
+
+    for (int i = 0; i < n; i++) cout << a[i] << " ";
+    return 0;
+}

e13e067748feff51b98d744be297db838e6c7ca2
Vishal <vishalvpatil45@gmail.com>
2026-01-12 22:07:06 +0530

    Quick Sort

---
+++ QuickSort.cpp
@@ -0,0 +1,43 @@
+//QuickSort implementation in C++
+#include <iostream>
+using namespace std;
+
+int partition(int a[], int low, int high) {
+    int pivot = a[high];
+    int i = low - 1;
+
+    for (int j = low; j < high; j++) {
+        if (a[j] <= pivot) {
+            i++;
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+
+    int temp = a[i + 1];
+    a[i + 1] = a[high];
+    a[high] = temp;
+
+    return i + 1;
+}
+
+void quickSort(int a[], int low, int high) {
+    if (low < high) {
+        int pi = partition(a, low, high);
+        quickSort(a, low, pi - 1);
+        quickSort(a, pi + 1, high);
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    quickSort(a, 0, n - 1);
+
+    for (int i = 0; i < n; i++) cout << a[i] << " ";
+    return 0;
+}

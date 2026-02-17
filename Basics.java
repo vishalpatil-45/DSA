2ecdc60a079526529b76463870aff38c73e32836
vishalpatil-45 <vishalvpatil45@gmail.com>
2026-02-17 10:24:33 +0530

    Exception Handling

---
+++ Basics.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Basics{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        a++;
+        System.out.println(a);
+        ++a;
+        System.out.println(a);
+    }
+}

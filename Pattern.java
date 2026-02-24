ebb236e0100e2380cd4a25e2ccf23e1bb25aef39
vishalpatil-45 <vishalvpatil45@gmail.com>
2026-02-25 02:25:40 +0530

    Patterns in Java

---
+++ Pattern.java
@@ -0,0 +1,17 @@
+//triangle of number pattern
+
+import java.util.Scanner;
+
+public class Pattern{
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}

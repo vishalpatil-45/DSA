fcad450d1977ada5df660bb5b90d2fef44ea2490
vishalpatil-45 <vishalvpatil45@gmail.com>
2026-02-25 02:47:59 +0530

    Diamond Pattern

--- Pattern.java
+++ Pattern.java
@@ -1,15 +1,17 @@
-//triangle of number pattern
-
+//pyramid of numbers
 import java.util.Scanner;
-
-public class Pattern{
-    public static void main(String[] args){
-        Scanner scn = new Scanner(System.in);
-        int n = scn.nextInt();
-
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=i; j++){
-                System.out.print(j + " ");
+public class Pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k + " ");
             }
             System.out.println();
         }
